69d153c04faf7c408fdd484e7eb8f0858c39d3e2
Gautam-Rathour <gau321tam@gmail.com>
2025-09-25 12:16:17 +0530

    Updated Code (can make rectangle)

--- draw-app/apps/excelidraw-frontend/app/draw/index.ts
+++ draw-app/apps/excelidraw-frontend/app/draw/index.ts
@@ -1,9 +1,23 @@
 
 
 
+type Shape = {
+    type: "rect";
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+} | {
+    type: "circle";
+    x: number;
+    y: number;
+    radius: number;
+}
+
 export function initDraw(canvas: HTMLCanvasElement) {
-            
             const ctx = canvas.getContext("2d");
+
+            let existingShapes: Shape[] = [];
 
             if(!ctx) {
                 return
@@ -26,6 +40,15 @@ export function initDraw(canvas: HTMLCanvasElement) {
 
             canvas.addEventListener("mouseup", (e) => {
                 clicked = false;
+                const width = e.clientX - startX;
+                const height = e.clientY - startY;
+                existingShapes.push({
+                    type: "rect",
+                    x: startX,
+                    y: startY,
+                    height,
+                    width
+                })
             })
 
 
@@ -33,11 +56,26 @@ export function initDraw(canvas: HTMLCanvasElement) {
                 if(clicked) {
                     const width = e.clientX - startX;
                     const height = e.clientY - startY;
-                    ctx.clearRect(0, 0, width, height);
-                    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-                    ctx.fillRect(0, 0, width, height);
+                    clearCanvas(existingShapes, canvas, ctx);
                     ctx.strokeStyle = "rgba(255, 255, 255, 1)";
                     ctx.strokeRect(startX, startY, width, height);
                 }
             })
 }
+
+
+function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRendringContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0, 0, 0)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    existingShapes.map((shape) => {
+        if(shape.type === "rect") {
+            ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
+}
+
+
+
